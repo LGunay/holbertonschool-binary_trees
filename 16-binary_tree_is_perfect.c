4498d64b8572be3f8abd14115e76a240ae5b3b28
root <gqasimova93@gmail.com>
2024-01-12 13:58:55 -0800

    checks if a binary tree is perfect

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - find count of binary tree leaves
+ *
+ * @tree: binary tree
+ *
+ * Return: count of leaves of tree
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	if (binary_tree_height(tree->left) || binary_tree_height(tree->right))
+	{
+		if (binary_tree_height(tree->left) - binary_tree_height(tree->right) == 0)
+			return (1);
+		else
+			return (0);
+	}
+	return (0);
+}
