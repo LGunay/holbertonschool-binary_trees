ed43b6a9b5d1856e581db1c87d7edff9783eef94
root <gqasimova93@gmail.com>
2024-01-12 14:08:04 -0800

    added condition for parent NULL

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -19,7 +19,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	return (0);
 }
 /**
- * binary_tree_perfect - checked perfect
+ * binary_tree_is_perfect - checked perfect
  *
  * @tree: binary tree
  *
@@ -29,6 +29,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
+	if (!tree->parent)
+		return (1);
 	if (binary_tree_height(tree->left) || binary_tree_height(tree->right))
 	{
 		if (binary_tree_height(tree->left) - binary_tree_height(tree->right) == 0)
