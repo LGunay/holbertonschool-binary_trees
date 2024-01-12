fb3025c9507745f19155050acf3235b5e5a648c3
root <gqasimova93@gmail.com>
2024-01-12 14:35:45 -0800

    added leaves function

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -45,16 +45,17 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	size_t leaves;
+	size_t leaves, height;
 
 	if (!tree)
 		return (0);
 	if (!tree->parent)
 		return (1);
+	height = binary_tree_height(tree);
 	if (binary_tree_height(tree->left) || binary_tree_height(tree->right))
 	{
 		if (binary_tree_height(tree->left) - binary_tree_height(tree->right) == 0 ||
-				leaves == 2 << binary_tree_height(tree))
+				leaves == 2 << height)
 			return (1);
 		else
 			return (0);
